96355e1229e66a0112ee6cba9d9b6c659fb078c0
cayllahuapedro <cayllahuapedro@gmail.com>
2024-10-23 17:16:30 -0300

    responsive design. Add dark/light themes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage:{
+				'landing-light': "url('/images/Bitcompay-BACKGROUND-White.jpg')",
+				'landing-dark': "url('/images/Bitcompay-BACKGROUND-Black-entero.jpg')",
+			}
+		},
 	},
 	plugins: [],
 }
