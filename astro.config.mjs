0641a3b65ad329d01dd21ec418b278b416f62493
brezza <brezza@dc.uba.ar>
2024-11-04 14:22:42 -0300

    attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig,envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
+  adapter: vercelServerless(),
   integrations: [tailwind()],
   experimental:{
     env: {
